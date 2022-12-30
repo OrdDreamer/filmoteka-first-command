8cbb855fe68685c6e399c5029b698a463b1bbe79
Oleksii Dembnovetskyi <dembnoveckii@gmail.com>
2022-12-31 00:42:17 +0100

    remove percents

--- src/js/Preloader.js
+++ src/js/Preloader.js
@@ -1,24 +1,10 @@
 export class Preloader {
   static refs = {
     loaderEl: document.querySelector('.preloader'),
-    percentsEl: document.querySelector('.percents'),
   };
 
   showLoader() {
     return Preloader.refs.loaderEl.classList.remove('preloader-invisible');
-  }
-
-  addLoader(data) {
-    console.log(data.length);
-    let i = 0;
-
-    data.forEach(file => {
-      i++;
-      console.log(file);
-
-      // percentsEl.innerHTML = ((100 / data.length) * i).toFixed(0);
-      Preloader.refs.percentsEl.innerHTML = ((i * 100) / data.length).toFixed(0);
-    });
   }
 
   hideLoader() {
@@ -31,5 +17,4 @@ export class Preloader {
 
 // вызов методов класса
 // loader.showLoader();
-// loader.addLoader(data.hits);
 // loader.hideLoader();
