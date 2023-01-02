04dc4307c36114cb343bc7f1db0b92a301704027
Bogdan <bogdanberezka@gmail.com>
2023-01-02 18:43:29 +0200

    add_anchor

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import FilmotekaAPI from './js/FilmotekaAPI';
 import Library from './js/Library';
-import { getFilmItemElement } from "./js/film-item";
-
-
+import { getFilmItemElement } from './js/film-item';
+import { anchors } from './js/anchor';
