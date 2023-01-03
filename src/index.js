cf4634b06784fa7c8c4c5922e2b0dc93a30cec7d
Bogdan <bogdanberezka@gmail.com>
2023-01-03 18:46:26 +0200

    add_hide_class_for_anchor

--- src/index.js
+++ src/index.js
@@ -2,3 +2,4 @@ import FilmotekaAPI from './js/FilmotekaAPI';
 import Library from './js/Library';
 import { getFilmItemElement } from './js/film-item';
 import { anchors } from './js/anchor';
+import { scrollpos } from './js/hideAnchor';
