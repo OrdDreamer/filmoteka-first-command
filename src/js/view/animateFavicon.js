f512662d5fbe0c501cd5d91cefa580f570793718
DolphiN1000 <88268032+DolphiN1000@users.noreply.github.com>
2023-01-09 03:44:13 +0200

    оптимізований код

--- src/js/view/animateFavicon.js
+++ src/js/view/animateFavicon.js
@@ -3,20 +3,17 @@ link_ = document.getElementById('favicon_');
 
 const a = link.getAttribute('href');
 const b = link_.getAttribute('href');
-let index = 0
 
 function animateFavicon() {
    
   timerId = setTimeout(() => {
     
-    if (index === 0) { 
+    if (a === link.getAttribute('href')) { 
       link.setAttribute('href', b);
       link_.setAttribute('href', a)
-      index = 1;
     } else {
       link.setAttribute('href', a);
       link_.setAttribute('href', b)
-      index = 0;
     };
     animateFavicon()
   }, 1000);
